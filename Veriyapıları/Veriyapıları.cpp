3916abedc53606a58ff1e6a9eff4121c61026e17
eminasci <eminasci@hotmail.com>
2022-03-25 13:08:58 +0300

    Add project files.

---
+++ Veriyapıları/Veriyapıları.cpp
@@ -0,0 +1,12 @@
+﻿// Veriyapıları.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "bir veri giriniz ";
+       
+
+/
